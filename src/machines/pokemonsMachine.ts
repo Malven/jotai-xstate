3254ed58f1e24d5a1ea54f09bfeea098ec7a6090
Marcus Alvén <marcus.alven@sgroup-solutions.se>
2023-08-01 08:43:53 +0200

    ts

--- src/machines/pokemonsMachine.ts
+++ src/machines/pokemonsMachine.ts
@@ -1,14 +1,21 @@
 import { assign, createMachine } from 'xstate';
 import type { PokemonBase } from '../todo/pokemon';
 import { getPokemons } from '../utils/api';
-export const pokemonsMachine = createMachine<{ pokemons: PokemonBase[] }>({
-  /** @xstate-layout N4IgpgJg5mDOIC5QAcD2BrMBbVA7WAdGAE7GrEDEASgKIAqVAmgNoAMAuoiqrAJYAuvPFxAAPRACYANCACeiALQBGAL4qZaTDnwEANqgCGEXrigUIeMARMA3DFc3Y8hfUZNQEt1AGMDgvGzsgSJofP64IuIISgCcABwErABsEgCsMvIIEjFKBAAsqQDMSmlqGvbaLobGphQkZMQEyLp+AGbkWE0VznrV7p64dr7hgcFIIKECQhHjURIA7HmJKelyiKwEqWrqILioEHAh3fhHYdORinF5GYqxCakShXmF81s7jpVEpOSnU8KziDihWWaRuCGUSlyrEez1eZQmxyqblMv3CF3BhSSYKUcSSBBi8ySqRW8I+PVgAFdvN44PBxpM0QCEHklKkQatMoVWGyYtkknE4dsgA */
+export const pokemonsMachine = createMachine({
   context: {
     pokemons: [],
   },
   id: 'pokemons',
+
   initial: 'loading',
+
   predictableActionArguments: true,
+
+  schema: {
+    context: {} as { pokemons: PokemonBase[] },
+    events: {} as { type: 'RETRY' },
+  },
+
   states: {
     error: {
       on: {
@@ -32,4 +39,6 @@ export const pokemonsMachine = createMachine<{ pokemons: PokemonBase[] }>({
       type: 'final',
     },
   },
+  /** @xstate-layout N4IgpgJg5mDOIC5QAcD2BrMBbVA7WAdGAE7GrEDEASgKIAqVAmgNoAMAuoiqrAJYAuvPFxAAPRACYANCACeiALQBGAL4qZaTDnwEANqgCGEXrigUIeMARMA3DFc3Y8hfUZNQEt1AGMDgvGzsgSJofP64IuIISgCcABwErABsEgCsMvIIEjFKBAAsqQDMSmlqGvbaLobGphQkZMQEyLp+AGbkWE0VznrV7p64dr7hgcFIIKECQhHjURIA7HmJKelyiKwEqWrqILioEHAh3fhHYdORinF5GYqxCakShXmF81s7jpVEpOSnU8KziDihWWaRuCGUSlyrEez1eZQmxyqblMv3CF3BhSSYKUcSSBBi8ySqRW8I+PVgAFdvN44PBxpM0QCEHklKkQatMoVWGyYtkknE4dsgA */
+  tsTypes: {} as import('./pokemonsMachine.typegen').Typegen0,
 });
